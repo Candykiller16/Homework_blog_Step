c478ac514fa0a88640b4ee7c3b771f501b9ab812
Candykiller16 <Candykiller16@mail.ru>
2022-01-16 18:23:53 +0300

    second update of blog. read README.md

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=150)
-    text = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # первичный ключ таблицы (генерируется автоматически при создании нового поста)
+    # с каскадным удалением всех записей при удалении автора
+    title = models.CharField(max_length=150) # текстовое поле ограниченное 150 исмволами
+    text = models.TextField() # текстовое поле, неограниченное размерами
+    created = models.DateTimeField(default=timezone.now) # исходя из названия, будет отображать время создания поста
+
+    def __str__(self): # для понятного отображения в админке
+        return self.title + ' | ' + str(self.author)  # т.к. author это объект, необходимо преобразовать его в строку
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def posts(request):
+def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts':posts})
-    # return HttpResponse('Posts')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-# def post(request):
-#     return HttpResponse('Post')
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
